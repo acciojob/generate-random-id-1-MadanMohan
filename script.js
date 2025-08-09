6f92b0b511a22be829af430092ba47c6c47d0ae2
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-09 19:12:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function makeid(l) {
-  // write your code here
+  let result = '';
+  
+  for (let i = 0; i < l; i++) {
+    // Pick either digit range or letter range
+    let asciicode;
+    if (Math.random() < 0.5) {
+      // Digit 0-9
+      asciicode = Math.floor(Math.random() * (57 - 48 + 1)) + 48;
+    } else {
+      // Uppercase A-Z
+      asciicode = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
+    }
+    result += String.fromCharCode(asciicode);
+  }
+  
+  return result;
 }
 
 // Do not change the code below.
